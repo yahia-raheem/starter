9268bb87075f54019cbe8aae49ca5af5ff61404e
Yahia Raheem <eng.yahiaraheem.93@outlook.com>
2020-11-04 15:09:59 +0200

    added some more theme supports in the helpers php file

--- functions/helpers.php
+++ functions/helpers.php
@@ -1,6 +1,47 @@
 <?php
 add_theme_support('post-thumbnails');
-add_theme_support( 'custom-logo' );
+add_theme_support('custom-logo');
+add_theme_support('menus');
+// Add default posts and comments RSS feed links to head.
+add_theme_support('automatic-feed-links');
+/*
+* Let WordPress manage the document title.
+* By adding theme support, we declare that this theme does not use a
+* hard-coded <title> tag in the document head, and expect WordPress to
+* provide it for us.
+*/
+add_theme_support('title-tag');
+
+/*
+* Switch default core markup for search form, comment form, and comments
+* to output valid HTML5.
+*/
+add_theme_support(
+    'html5',
+    array(
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    )
+);
+/*
+* Enable support for Post Formats.
+*
+* See: https://codex.wordpress.org/Post_Formats
+*/
+add_theme_support(
+    'post-formats',
+    array(
+        'aside',
+        'image',
+        'video',
+        'quote',
+        'link',
+        'gallery',
+        'audio',
+    )
+);
 function _themename_customization($wp_customize) {
     $wp_customize->add_section('social_media_section', array('title' => 'Social Media', ));
     $wp_customize->add_setting('facebook', array('default' => '', ));
