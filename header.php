c0e42186f621be05bae59bda273b78a4821aafb4
yahia-raheem <eng.yahiaraheem.93@outlook.com>
2021-03-09 15:25:00 +0200

    removed reference to undeclared image size

--- header.php
+++ header.php
@@ -40,7 +40,7 @@
             <div class="row">
                 <div class="col-12">
                     <nav class="navbar navbar-expand-lg navbar-dark">
-                        <a class="navbar-brand" href="#"><?php echo wp_get_attachment_image($custom_logo_id, 'site-logo', true, ['class' => 'site-logo']); ?></a>
+                        <a class="navbar-brand" href="#"><?php echo wp_get_attachment_image($custom_logo_id, 'thumbnail', true, ['class' => 'site-logo']); ?></a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
